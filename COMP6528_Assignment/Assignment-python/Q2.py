16ab42745ca9c403eb560877664e1d908f4832e0
caiwa <caiwangshu@gmail.com>
2023-06-12 22:35:20 +1000

    substitute with pdf

--- COMP6528_Assignment/Assignment-python/Q2.py
+++ COMP6528_Assignment/Assignment-python/Q2.py
@@ -1,35 +1,36 @@
 import numpy as np
 import cv2
 
-I = np.array([[128, 128, 100, 100, 103,  50],
-                [150, 100, 120,  30,  53,  54],
-                [150, 112, 127,  40,  35,  20],
-                [132, 125, 112,  43,  20,  10],
-                [133, 130, 100,  30,  10,  20],
-                [140, 130, 120,  20,  10,  20]])
-k = 3
-l = 2
-def w_range(I,k,l,size=5, sigma = 50):
+I = np.array([[58,38,36],
+             [32,52,32],
+              [80,30,35]])
+k = 1
+l = 1
+def w_range(I,k,l,size=3, sigma = 50):
     # initialize a zero matrix with shape (size, size)
     ans = np.zeros((size,size))
     # traverse through the filtered region in I
     for i in range(k-size//2,k+size//2+1):
         for j in range(l-size//2,l+size//2+1):
             # update the zero matrix with value calculated by current traversing and central element
-            ans[i-k+size//2,j-l+size//2] = np.exp(-np.linalg.norm(I[i,j]-I[k,l])**2/2/sigma**2)
+            ans[i-k+size//2,j-l+size//2] = np.exp(-np.linalg.norm(I[i,j]-I[k,l])**2/(2*sigma**2))
     return ans
 # generate range filter
-r_filter = w_range(I,3,2)
-
-domain_filter = np.array([[0.0232, 0.0338, 0.0383, 0.0338, 0.0232],
-                [0.0338, 0.0492, 0.0558, 0.0492, 0.0338],
-                [0.0383, 0.0558, 0.0632, 0.0558,  0.0383],
-                [0.0338, 0.0492, 0.0558, 0.0492, 0.0338],
-                [0.0232, 0.0338, 0.0383, 0.0338, 0.0232]])
-def filterer_value(I,k,l,size=5):
+r_filter = w_range(I,1,1)
+print(r_filter)
+def gkern(l=5, sig=1.):
+    """\
+    creates gaussian kernel with side length `l` and a sigma of `sig`
+    """
+    ax = np.linspace(-(l - 1) / 2., (l - 1) / 2., l)
+    gauss = np.exp(-0.5 * np.square(ax) / np.square(sig))
+    kernel = np.outer(gauss, gauss)
+    return kernel / np.sum(kernel)
+domain_filter = gkern(3,2)
+def filterer_value(I,k,l,size=3):
     # obtain domain and range filter
     domain_kernel = domain_filter
-    range_filter = w_range(I,3,2)
+    range_filter = w_range(I,1,1)
     # calculate wp by summing up the elementwise product of the two filters
     wp = np.sum(domain_kernel*range_filter)
     # calculate the filtered matrix
@@ -37,4 +38,4 @@ def filterer_value(I,k,l,size=5):
     # summing up to obtain filtered value
     return np.sum(ans)
 # generate filtered value
-f_value = filterer_value(I,3,2)
+f_value = filterer_value(I,1,1)
